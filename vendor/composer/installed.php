dac6f9305f795028c1d44893b51b68183924940a
Christos Athanasiadis <chris.k.athanasiadis@gmail.com>
2024-02-13 16:34:43 +0000

    chore(release): 1.0.10

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'merge-org/sort',
-        'pretty_version' => '1.0.9',
-        'version' => '1.0.9.0',
+        'pretty_version' => '1.0.10',
+        'version' => '1.0.10.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'merge-org/sort' => array(
-            'pretty_version' => '1.0.9',
-            'version' => '1.0.9.0',
+            'pretty_version' => '1.0.10',
+            'version' => '1.0.10.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
