6e38780f51f6fac1210333fead90fb0035133394
Christos Athanasiadis <chris.k.athanasiadis@gmail.com>
2024-02-27 10:07:51 +0000

    chore(release): 1.0.39

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * Description: 📊Sort - Sales Order Ranking Tool | Powered by Merge
  * Author: Merge
  * Author URI: https://github.com/merge-org
- * Version: 1.0.38
+ * Version: 1.0.39
  * Text Domain: merge-org-sort
  * Domain Path: /languages
  * Requires PHP: 7.4
@@ -22,13 +22,16 @@ namespace MergeOrg\Sort;
 use MergeOrg\Sort\Exception\SortException;
 use MergeOrg\Sort\WordPress\ActionsRegistrar;
 
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . '/vendor/autoload.php';
 
 try {
 	ActionsRegistrar::construct();
-} catch(SortException $sortException) {
-	add_action("admin_notices", function() use ($sortException) {
-		echo "<div class='notice notice-error'><p>Sort | Error: {$sortException->getMessage()}{$sortException->getTraceAsString()}</p></div>";
-	});
+} catch ( SortException $sortException ) {
+	add_action(
+		'admin_notices',
+		function () use ( $sortException ) {
+			echo "<div class='notice notice-error'><p>Sort | Error: {$sortException->getMessage()}{$sortException->getTraceAsString()}</p></div>";
+		}
+	);
 	return;
 }
