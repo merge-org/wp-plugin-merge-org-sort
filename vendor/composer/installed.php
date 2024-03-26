4dcc5288b8eef1a80ad36ff107e81e6e390b01b4
Christos Athanasiadis <chris.k.athanasiadis@gmail.com>
2024-03-26 18:02:38 +0000

    chore(release): 2.3.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'merge-org/wp-plugin-merge-org-sort',
-        'pretty_version' => '2.3.0',
-        'version' => '2.3.0.0',
+        'pretty_version' => '2.3.1',
+        'version' => '2.3.1.0',
         'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'merge-org/wp-plugin-merge-org-sort' => array(
-            'pretty_version' => '2.3.0',
-            'version' => '2.3.0.0',
+            'pretty_version' => '2.3.1',
+            'version' => '2.3.1.0',
             'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
